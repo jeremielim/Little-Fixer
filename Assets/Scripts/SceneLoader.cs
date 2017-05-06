7d73f1b96d8533146a27edce30ff9ed3906dadd2
Jeremie Lim <hello@jeremiespoken.com>
2017-05-06 11:42:24 -0700

    Add default structure

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
