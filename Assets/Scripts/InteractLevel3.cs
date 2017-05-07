a95b5d501aef930a7c3a503ce7532ed09ecef16c
Jeremie Lim <hello@jeremiespoken.com>
2017-05-07 13:56:30 -0700

    Finish level 3 logic

--- Assets/Scripts/InteractLevel3.cs
+++ Assets/Scripts/InteractLevel3.cs
@@ -6,4 +6,33 @@ public class InteractLevel3 : Interact
 {
     public Text dialog;
     public GameObject nextButton;
+    public GameObject mango;
+
+    public GameObject cryBabyEye;
+    public GameObject cryBabyMouth;
+
+    private Animator cryBabyEyeAnimator;
+    private Animator cryBabyMouthAnimator;
+
+    void Start()
+    {
+        cryBabyEyeAnimator = cryBabyEye.GetComponent<Animator>();
+        cryBabyMouthAnimator = cryBabyMouth.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (mango != null)
+        {
+            if (mango.transform.position.y < -1.961f && mango.transform.position.x > 1.53f)
+            {
+                Destroy(mango);
+                cryBabyEyeAnimator.SetTrigger("isHappy");
+                cryBabyMouthAnimator.SetTrigger("isHappy");
+
+                dialog.text = "Yey!";
+                nextButton.SetActive(true);
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/PlayMusicOverScenes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayMusicOverScenes : MonoBehaviour
+{
+
+    static bool AudioBegin = false;
+
+    void Awake()
+    {
+        if (!AudioBegin)
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+}
